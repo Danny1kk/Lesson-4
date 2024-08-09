a88f9b45c5ffe139277f8b21d8563e4c4b264177
Danny1kk <danny.koshik@gmail.com>
2024-08-09 21:33:43 +0300

    Add files via upload

--- module_1_4.py
+++ module_1_4.py
@@ -1,5 +1,6 @@
 horror_movie = input('Введите Ваш любимый фильм ужасов: ')
-print(horror_movie.count(''))
+#print(horror_movie.count(''))
+print(len(horror_movie))
 print(horror_movie.upper())
 print(horror_movie.lower())
 print(horror_movie.replace(' ', ''))
