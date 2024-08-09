b3b7f02f409f95ccb12a302d21558328afa541a7
Danny1kk <danny.koshik@gmail.com>
2024-08-09 20:18:41 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+horror_movie = input('Введите Ваш любимый фильм ужасов: ')
+print(horror_movie.count(''))
+print(horror_movie.upper())
+print(horror_movie.lower())
+print(horror_movie.replace(' ', ''))
+print(horror_movie[0])
+print(horror_movie[-1])
+
+#Техасская резня бензапилой
